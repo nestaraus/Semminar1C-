ecf35ca0d49a2f3f38a509f48e4efc0a52bd140d
nestaraus <nestaraus@mail.pu>
2023-09-24 14:16:53 +0300

    Zadaqcha 1

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿//comment
+/* mnogostrochniy
+dfdf
+*/
+// ctrl + /
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+// Например:
+//  4-> 16
+// -3-> 9
+// -7 -> 49
+
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n * n);
