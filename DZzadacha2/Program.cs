d81089fba08fd0687ca88149eddf3ff2bcd41263
nestaraus <nestaraus@mail.pu>
2023-09-24 15:27:33 +0300

    Dz2

---
+++ DZzadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("max = " + a);
+    Console.Write("min = " + b);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+    Console.Write("min = " + a);
+}
