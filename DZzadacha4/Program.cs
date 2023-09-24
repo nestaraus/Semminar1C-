0ffd136a1c1aa0d7af0b1111ff6b1727b8b32bff
nestaraus <nestaraus@mail.pu>
2023-09-24 15:45:12 +0300

    Dz4

---
+++ DZzadacha4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    if (a>c)
+    {
+        Console.WriteLine("max = " + a);
+    }
+
+} 
+else if (b>a)
+{
+    if (b>c)
+    {
+        Console.WriteLine("max = " + b);
+    }
+
+} 
+if (c>a)
+{
+    if (c>b)
+    {
+        Console.WriteLine("max = " + c);
+    }
+
+} 
+
+
